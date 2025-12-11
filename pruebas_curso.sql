5af589fce477b065635e07c09e5fbec813894ff3
Carlos Rodríguez <145693019+44devCarlos@users.noreply.github.com>
2025-12-10 19:10:03 -0500

    Base de datos actualizada 4

--- pruebas_curso.sql
+++ pruebas_curso.sql
@@ -20,6 +20,97 @@ DROP DATABASE IF EXISTS `pruebas_curso`;
 CREATE DATABASE IF NOT EXISTS `pruebas_curso` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `pruebas_curso`;
 
+-- Dumping structure for procedure pruebas_curso.actualizar_usuario
+DROP PROCEDURE IF EXISTS `actualizar_usuario`;
+DELIMITER //
+CREATE PROCEDURE `actualizar_usuario`(
+	IN `p_id_usuario` INT,
+	IN `p_email` TEXT,
+	IN `p_contrasena` TEXT,
+	IN `p_locked` INT,
+	IN `p_failed_attempt` INT,
+	IN `p_token` TEXT
+)
+BEGIN
+	 DECLARE email_actual VARCHAR(100);
+    DECLARE email_existe INT;
+
+    -- Obtener el email actual
+    SELECT email INTO email_actual FROM usuarios WHERE id_usuario = p_id_usuario;
+
+    -- Verificar si se intenta cambiar el email
+    IF p_email IS NOT NULL AND p_email != email_actual THEN
+        SELECT COUNT(*) INTO email_existe
+        FROM usuarios
+        WHERE email = p_email AND id_usuario != p_id_usuario;
+
+        IF email_existe > 0 THEN
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Este email ya está registrado por otro usuario';
+        END IF;
+    END IF;
+
+    -- Si se proporciona una nueva contraseña, se actualiza con ella
+    IF p_contrasena IS NOT NULL AND p_contrasena != '' THEN
+        UPDATE usuarios
+        SET email = IF(p_email IS NOT NULL AND p_email != '', p_email, email_actual),
+            contrasena = p_contrasena, usuarios.`locked` = p_locked, 
+				usuarios.failed_attempt = p_failed_attempt, token = p_token
+        WHERE id_usuario = p_id_usuario;
+    ELSE
+        UPDATE usuarios
+        SET email = IF(p_email IS NOT NULL AND p_email != '', p_email, email_actual), 
+				usuarios.`locked` = p_locked, usuarios.failed_attempt = p_failed_attempt, token = p_token
+        WHERE id_usuario = p_id_usuario;
+    END IF;
+END//
+DELIMITER ;
+
+-- Dumping structure for procedure pruebas_curso.actualizar_usuario_completo
+DROP PROCEDURE IF EXISTS `actualizar_usuario_completo`;
+DELIMITER //
+CREATE PROCEDURE `actualizar_usuario_completo`(
+	IN `p_id_usuario` INT,
+	IN `p_contrasena` TEXT,
+	IN `p_locked` INT,
+	IN `p_failed_attempt` INT,
+	IN `p_token` TEXT,
+	IN `p_token_reset` TEXT,
+	IN `p_token_expira` DATETIME
+)
+BEGIN
+	UPDATE usuarios AS u
+	SET u.contrasena = p_contrasena, u.`locked` = p_locked, u.failed_attempt = p_failed_attempt,
+	u.token = p_token, u.token_reset = p_token_reset, u.token_expira = p_token_expira
+	WHERE u.id_usuario = p_id_usuario;
+END//
+DELIMITER ;
+
+-- Dumping structure for procedure pruebas_curso.consultar_usuario
+DROP PROCEDURE IF EXISTS `consultar_usuario`;
+DELIMITER //
+CREATE PROCEDURE `consultar_usuario`(
+	IN `p_email` TEXT
+)
+BEGIN
+	SELECT * FROM usuarios WHERE email = p_email;
+END//
+DELIMITER ;
+
+-- Dumping structure for procedure pruebas_curso.guardar_token_recuperacion
+DROP PROCEDURE IF EXISTS `guardar_token_recuperacion`;
+DELIMITER //
+CREATE PROCEDURE `guardar_token_recuperacion`(
+	IN `p_id_usuario` INT,
+	IN `p_token_reset` TEXT,
+	IN `p_token_expira` DATETIME
+)
+BEGIN
+	UPDATE usuarios AS u
+	SET u.token_reset = p_token_reset, u.token_expira = p_token_expira
+	WHERE id_usuario = p_id_usuario;
+END//
+DELIMITER ;
+
 -- Dumping structure for table pruebas_curso.usuarios
 DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE IF NOT EXISTS `usuarios` (
@@ -29,10 +120,29 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `locked` int DEFAULT NULL,
   `failed_attempt` int DEFAULT NULL,
   `token` text COLLATE utf8mb4_general_ci,
+  `token_reset` text COLLATE utf8mb4_general_ci,
+  `token_expira` datetime DEFAULT NULL,
   PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table pruebas_curso.usuarios: ~0 rows (approximately)
+-- Dumping data for table pruebas_curso.usuarios: ~1 rows (approximately)
+INSERT INTO `usuarios` (`id_usuario`, `email`, `contrasena`, `locked`, `failed_attempt`, `token`, `token_reset`, `token_expira`) VALUES
+	(1, 'carv2012@gmail.com', '12345678', 0, 0, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNhcnYyMDEyQGdtYWlsLmNvbSIsImlhdCI6MTc2NTMzNDQyMywiZXhwIjoxNzY1MzM1MDIzfQ._9WK5vCIRONHN-L_u0QEiu0MMIHp3ExI_pzg7Plrw2c', NULL, NULL);
+
+-- Dumping structure for procedure pruebas_curso.verificar_usuario
+DROP PROCEDURE IF EXISTS `verificar_usuario`;
+DELIMITER //
+CREATE PROCEDURE `verificar_usuario`(
+	IN `p_email` TEXT,
+	IN `p_contrasena` TEXT
+)
+BEGIN
+	SELECT *
+    FROM usuarios
+    WHERE email = p_email AND contrasena = p_contrasena
+    LIMIT 1;
+END//
+DELIMITER ;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
